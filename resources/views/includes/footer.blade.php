3f38867ecd7edc6e836c04d38057f5d8ab5acc22
Fredrick Otim <fredrickot@gmail.com>
2018-12-31 19:53:08 +0300

    styles footer section

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,50 +1,64 @@
 <div class="row">
     <div class="col-1-of-4">
-        <div class="content">
-            <h1>Heading</h1>
-            <p>
-                Lorem ipsum dolor sit amet, an his etiam torquatos.
-                Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur.
-                Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.
-                Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.
-            </p>
-            <button class="btn">Love</button>
+        <div class="content mt-md">
+            <h1 class="mb-sm heading-4">Projects</h1>
+            <ul class="ul__footer-list">
+                <li class="mb-sm heading-5">
+                    <a href="#">Open source</a>
+                </li>
+                <li class="mb-sm heading-5">
+                    <a href="#">Personal</a>
+                </li>
+            </ul>
         </div>
     </div>
     <div class="col-1-of-4">
-        <div class="content">
-            <h1>Heading</h1>
-            <p>
-                Lorem ipsum dolor sit amet, an his etiam torquatos.
-                Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur.
-                Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.
-                Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.
-            </p>
-            <button class="btn">Love</button>
+        <div class="content mt-md">
+            <h1 class="mb-sm heading-4">Articles</h1>
+            <ul class="ul__footer-list">
+                <li class="mb-sm heading-5">
+                    <a href="#">Technology</a>
+                </li>
+                <li class="mb-sm heading-5">
+                    <a href="#">Productivity</a>
+                </li>
+                <li class="mb-sm heading-5">
+                    <a href="#">Entreprenuership</a>
+                </li>
+            </ul>
         </div>
     </div>
     <div class="col-1-of-4">
-        <div class="content">
-            <h1>Heading</h1>
-            <p>
-                Lorem ipsum dolor sit amet, an his etiam torquatos.
-                Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur.
-                Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.
-                Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.
-            </p>
-            <button class="btn">Love</button>
+        <div class="content mt-md">
+            <h1 class="mb-sm heading-4">Contact</h1>
+            <ul class="ul__footer-list">
+                <li class="mb-sm heading-5">
+                    business@otimfredrick.com
+                </li>
+                <li class="mb-sm heading-5">
+                    (+256) 706 248 225
+                </li>
+            </ul>
         </div>
     </div>
     <div class="col-1-of-4">
-        <div class="content">
-            <h1>Heading</h1>
-            <p>
-                Lorem ipsum dolor sit amet, an his etiam torquatos.
-                Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur.
-                Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.
-                Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.
-            </p>
-            <button class="btn">Love</button>
+        <div class="content mt-md">
+            <h1 class="mb-sm heading-4">Connect </h1>
+            <div class="wrapper">
+               <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-github"></i></a>
+               <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-twitter"></i></a>
+               <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-linkedin"></i></a>
+               <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-google-plus"></i></a>
+               <a target="_blank" href="your_url_here"><i class="fa fa-3x fa-facebook"></i></a>
+            </div>
         </div>
     </div>
+</div>
+
+<hr class="hr__footer" data-content="WITH LOVE">
+
+<div class="row">
+    <p class="copyright mt-sm mb-sm heading-6">
+        Desined and Built by Otim Fredrick, Uganda. &copy; Copyright <?php echo date("Y"); ?>, <a href="#">Terms of Use.</a> | <a href="#">Privacy Policy</a>.
+    </p>
 </div>
