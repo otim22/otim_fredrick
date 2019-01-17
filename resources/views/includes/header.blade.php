98063ad22e6266a2a090616f24e1affe7c0f2c0c
Fredrick Otim <fredrickot@gmail.com>
2019-01-17 15:47:45 +0300

    implements contact page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,4 @@
 <div class="header">
-    <!-- Header -->
-    <!-- <video class="bg-video__content" autoplay muted loop>
-        <source src="public/img/header.mp4" type="video/mp4">
-        <source src="public/img/header.webm" type="video/webm">
-            Your browser is not supported.
-    </video> -->
     <div class="header__text">
         <h3 class="heading-2 mb-md header__text-1">Lorem ipsum dolor sit amet, an his etiam torquatos.</h3>
         <h1 class="heading-3 mb-lg header__text-2">Lorem ipsum dolor sit amet, an his etiam torquatos.
@@ -12,5 +6,4 @@
         Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.</h1>
         <button class="btn btn--white btn--animated btn__header">Read more</button>
     </div>
-
 </div>
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,7 +4,52 @@
 @section('content')
 
 <section class="section-contact">
-      i am the contact page
+    <div class="section-contact">
+        <div class="row">
+            <div class="col-1-of-2">
+				<div class="contact-img">
+					<img src="{{ asset('img/img-01.png') }}" alt="Contact Image">
+				</div>
+            </div>
+
+            <div class="col-1-of-2">
+                <div class="contact-form">
+                    <form class="contact-form">
+                        <span class="contact-form-title">
+                            Get in touch
+                        </span>
+
+                        <div class="wrap-input validate-input" data-validate = "Name is required">
+                            <input class="input-field" type="text" name="name" placeholder="Name">
+                            <span class="focus-input"></span>
+                            <span class="symbol-input">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                            </span>
+                        </div>
+
+                        <div class="wrap-input validate-input" data-validate = "Valid email is required: account@example.xyz">
+                            <input class="input-field" type="text" name="email" placeholder="Email">
+                            <span class="focus-input"></span>
+                            <span class="symbol-input">
+                                <i class="fa fa-envelope" aria-hidden="true"></i>
+                            </span>
+                        </div>
+
+                        <div class="wrap-input validate-input" data-validate = "Message is required">
+                            <textarea class="input-field" name="message" placeholder="Message"></textarea>
+                            <span class="focus-input"></span>
+                        </div>
+
+                        <div class="container-contact-form-btn">
+                            <button class="btn contact-form-btn">
+                                Send
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 @stop
