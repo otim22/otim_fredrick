e18ac71477d9f570493b231d95f8dad5fec889b9
Fredrick Otim <fredrickot@gmail.com>
2019-01-17 17:03:39 +0300

    layout of the different resources

--- resources/views/pages/resources.blade.php
+++ resources/views/pages/resources.blade.php
@@ -3,7 +3,153 @@
 @section('content')
 
 <section class="section-resources">
-    i am the resources page
+    <div class="resources-header">
+        <div class="row resources-header__row">
+            Lorem ipsum dolor sit amet, an his etiam torquatos.
+            Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur.
+            Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei.
+        </div>
+    </div>
+    <div class="resource-title">
+        <div class="row">
+            <div class="col-1-of-3">
+                <div class="content mt-lg">
+                    <p class="heading-3">
+                        Development tools.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-1-of-2">
+                <div class="content mt-lg">
+                    i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                    ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                    anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1-of-2">
+            <div class="content mt-lg">
+                i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </div>
+        </div>
+        <div class="col-1-of-2">
+            <div class="content mt-lg">
+                Resources on html5
+            </div>
+        </div>
+    </div>
+
+    <div class="resource-title">
+        <div class="row">
+            <div class="col-1-of-3">
+                <div class="content mt-lg">
+                    Resources on css
+                </div>
+            </div>
+
+            <div class="col-1-of-2">
+                <div class="content mt-lg">
+                    i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1-of-2">
+            <div class="content mt-lg">
+                i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </div>
+        </div>
+
+        <div class="col-1-of-2">
+            <div class="content mt-lg mb-lg">
+                Resources on javascript.
+                Collection of Fonts and Typography
+            </div>
+        </div>
+    </div>
+
+    <div class="resource-title">
+        <div class="row">
+            <div class="col-1-of-3">
+                <div class="content mt-lg">
+                    Collection of Fonts and Typography
+                </div>
+            </div>
+
+            <div class="col-1-of-2">
+                <div class="content mt-lg">
+                    i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-1-of-2">
+            <div class="content mt-lg">
+                i am the resources page Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum.
+            </div>
+        </div>
+
+        <div class="col-1-of-2">
+            <div class="content mt-lg mb-lg">
+                Myriad of Colors.
+            </div>
+        </div>
+    </div>
+
+    <div class="subcription">
+        <div class="row subcription__row">
+            <h1 class="heading-3 mb-sm">Stay tune for updates</h1>
+            <h1 class="heading-5 mb-lg">Inspiring and thoughtful emails only.</h1>
+            <form action="#" class="subcription__menu">
+                <input type="email" class="subcription__menu-input" placeholder="name@example.com">
+                <button class="subcription__menu-button">
+                    Subcribe
+                </button>
+            </form>
+        </div>
+    </div>
 </section>
 
 @stop
